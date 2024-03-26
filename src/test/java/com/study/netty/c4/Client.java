f5ac6536ff4d8edfd176942188e2a0696c8d7f89
Xuyong <1299461580@qq.com>
2024-03-26 23:36:38 +0800

    changes

--- src/test/java/com/study/netty/c4/Client.java
+++ src/test/java/com/study/netty/c4/Client.java
@@ -3,9 +3,10 @@ package com.study.netty.c4;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void m2(String[] args) {
         try (Socket socket = new Socket("localhost", 8080)) {
             System.out.println(socket);
             socket.getOutputStream().write("world".getBytes());
@@ -16,10 +17,13 @@ public class Client {
         }
     }
 
-    public static void m1(String[] args) {
+    public static void main(String[] args) {
         try (SocketChannel sc = SocketChannel.open()) {
             sc.connect(new InetSocketAddress("localhost", 8080));
             System.out.println("waiting...");
+            sc.write(Charset.defaultCharset().encode("0123\n456789abcdef\n"));
+//            sc.write(Charset.defaultCharset().encode("012345\n6789abcdef333333\n"));
+            System.in.read();
         } catch (Exception e) {
             e.printStackTrace();
         }
