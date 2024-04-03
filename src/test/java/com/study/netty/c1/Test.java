cedbee9cbc472f0f10fd818be407d54870a72fd7
erye0 <1299461580@qq.com>
2024-04-03 14:19:38 +0800

    changes

--- src/test/java/com/study/netty/c1/Test.java
+++ src/test/java/com/study/netty/c1/Test.java
@@ -1,7 +1,9 @@
 package com.study.netty.c1;
 
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -9,6 +11,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,5 +46,21 @@ public class Test {
                     }
                 })
                 .bind(8080); // 4
+    }
+
+    public void test() throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup()) // 1
+                .channel(NioSocketChannel.class) // 2
+                .handler(new ChannelInitializer<Channel>() { // 3
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder()); // 8
+                    }
+                })
+                .connect("127.0.0.1", 8080) // 4
+                .sync() // 5
+                .channel() // 6
+                .writeAndFlush(new Date() + ": hello world!"); // 7
     }
 }
