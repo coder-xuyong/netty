0203be1a18bf74f52ed61d2e451607da6b736f69
erye0 <1299461580@qq.com>
2024-04-17 19:40:34 +0800

    changes:学海无涯苦作舟

--- src/test/java/com/study/netty/c5/WorkerServer.java
+++ src/test/java/com/study/netty/c5/WorkerServer.java
@@ -5,9 +5,12 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -15,7 +18,36 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class WorkerServer {
+
     public static void main(String[] args) {
+        DefaultEventLoopGroup normalWorkers = new DefaultEventLoopGroup(2);
+        try {
+            new ServerBootstrap()
+                    .group(new NioEventLoopGroup(1), new NioEventLoopGroup(2))
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch)  {
+                            ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                            ch.pipeline().addLast(normalWorkers,"myhandler",
+                                    new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                            ByteBuf byteBuf = msg instanceof ByteBuf ? ((ByteBuf) msg) : null;
+                                            if (byteBuf != null) {
+                                                byte[] buf = new byte[16];
+                                                ByteBuf len = byteBuf.readBytes(buf, 0, byteBuf.readableBytes());
+                                                log.debug(new String(buf));
+                                            }
+                                        }
+                                    });
+                        }
+                    }).bind(8080).sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void m1(String[] args) {
         try {
             new ServerBootstrap()
                     .group(new NioEventLoopGroup(1), new NioEventLoopGroup(2))
